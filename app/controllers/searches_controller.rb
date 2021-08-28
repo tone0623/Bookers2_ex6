b30e13422b8c76a5937d8b62591335f7d4631692
tone0623 <tone0623@icloud.com>
2021-08-28 07:07:18 +0000

    [Fix]検索機能改善

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,8 @@ class SearchesController < ApplicationController
     
     def search
         @search_name = params[:search]
+        @search_method = params[:search_method]
+        @search_model = params[:search_model]
         @results = User.where(name: @search_name)
     end
 end
